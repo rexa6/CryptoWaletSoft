40f8567467d9e2c626039ef4bc172f76c61bff28
rexa6 <visastoner@gmail.com>
2025-06-09 06:43:21 +0200

    Добавьте файлы проекта.

---
+++ WpfApp4/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GeneratorToggle.Checked += (s, e) =>
+            {
+                GeneratorSubmenu.Visibility = Visibility.Visible;
+                GeneratorToggle.Content = "Brute Force ▲";
+            };
+
+            GeneratorToggle.Unchecked += (s, e) =>
+            {
+                GeneratorSubmenu.Visibility = Visibility.Collapsed;
+                GeneratorToggle.Content = "Brute Force ▼";
+            };
+        }
+
+        private void ParserButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ParserView(); // ← вставляется прямо в MainWindow
+        }
+
+
+        private void BtcButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Генерация BTC кошелька", FontSize = 18, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+        }
+
+        private void LtcButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Генерация LTC кошелька", FontSize = 18, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+        }
+
+        private void TrxButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Генерация TRX кошелька", FontSize = 18, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+        }
+    }
+}
